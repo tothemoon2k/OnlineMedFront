2424649fde0aaaa43a824fd22608ddf6d2694c75
Justin Garner <justingarner@Justins-MBP.lan>
2024-10-20 18:48:18 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css';
+import Landing from './Landing';
+
+function App() {
+  return (
+    <Landing />
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='w-screen h-10'>
+            <div>
+                <a href="">ulfiry</a>
+                <a href="">Home</a>
+                <a href="">FAQ</a>
+            </div>
+
+            <button>Get Your Doctors Note</button>
+        </nav>
+    );
+};
+
+export default Navbar;
