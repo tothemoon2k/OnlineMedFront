40b4ea4f0d522a2e1f66e48733197379b437e743
Justin Garner <justin2013pdx@gmail.com>
2024-10-23 15:11:48 -0400

    Debug google conversion tracker

--- src/routes/Redirect.jsx
+++ src/routes/Redirect.jsx
@@ -1,39 +1,40 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import {Helmet} from "react-helmet";
 
 const Redirect = () => {
     const navigate = useNavigate();
 
-    useEffect(() => {
-        // Create the first script element
-        const script1 = document.createElement('script');
-        script1.src = 'https://www.googletagmanager.com/gtag/js?id=AW-16749261252';
-        script1.async = true;
+    // useEffect(() => {
+    //     window.location.href = 'https://xhj91lsy6tk.typeform.com/to/hnIsFmiP';
+    // }, [navigate]);
 
-        // Create the second script element
-        const script2 = document.createElement('script');
-        script2.innerHTML = `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-16749261252');
-        `;
+    return (
+        <div>
+            <Helmet>
+                <title>Redirecting...</title>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16749261252"></script>
+                <script type="text/javascript">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
 
-        // Append both scripts to the document head
-        document.head.appendChild(script1);
-        document.head.appendChild(script2);
-
-        // Redirect the user to the Typeform link
-        window.location.href = 'https://xhj91lsy6tk.typeform.com/to/hnIsFmiP';
-
-        // Cleanup function to remove the scripts when the component is unmounted
-        return () => {
-            document.head.removeChild(script1);
-            document.head.removeChild(script2);
-        };
-    }, [navigate]);
-
-    return null;
+                    gtag('config', 'AW-16749261252');
+                `}
+                </script>
+                <script type="text/javascript">
+                {`
+                    gtag('event', 'conversion', {
+                        'send_to': 'AW-16749261252/mfBOCLOu7-AZEMTj1bI-',
+                        'value': 1.0,
+                        'currency': 'USD'
+                    });
+                `}
+                </script>
+            </Helmet>
+        </div>
+    );
 };
 
 export default Redirect;
