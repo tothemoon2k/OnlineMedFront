25e7e41ce282d0d1b39caa0d59087cccfeb2bc9b
Justin Garner <justin2013pdx@gmail.com>
2024-10-23 01:41:55 -0400

    Add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import './App.css';
 import Landing from './Landing';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
-    <Landing />
+    <>
+      <Landing />
+      <Analytics />
+    </>
   )
 }
 
