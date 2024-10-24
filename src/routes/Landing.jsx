d1681722879ca6dd09f45dadc242ce90dc4ed357
Justin Garner <justin2013pdx@gmail.com>
2024-10-24 00:27:29 -0400

    Reduce bg image size, tweak footer

--- src/routes/Landing.jsx
+++ src/routes/Landing.jsx
@@ -171,7 +171,7 @@ const Landing = () => {
 
             <footer className="mt-32 mb-8 w-[85%] mx-auto bg-white rounded-lg shadow">
                 <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+                <span className="text-sm text-gray-500 sm:text-center">© 2024 <a href="https://flowbite.com/" className="hover:underline">OnlineMed™</a>. All Rights Reserved.
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
                     <li>
