a6c0e7adccec5d6ebb400e2212a4dd786bb6bab1
Justin Garner <justin2013pdx@gmail.com>
2024-10-30 18:22:12 -0400

    Add conversion tracking to thank you page

--- src/routes/Redirect.jsx
+++ src/routes/Redirect.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
-import {Helmet} from "react-helmet";
 
 const Redirect = () => {
     const navigate = useNavigate();
@@ -12,27 +11,6 @@ const Redirect = () => {
 
     return (
         <div>
-            <Helmet>
-                <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16749261252"></script>
-                <script type="text/javascript">
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'AW-16749261252');
-                `}
-                </script>
-                <script type="text/javascript">
-                {`
-                    gtag('event', 'conversion', {
-                        'send_to': 'AW-16749261252/mfBOCLOu7-AZEMTj1bI-',
-                        'value': 1.0,
-                        'currency': 'USD'
-                    });
-                `}
-                </script>
-            </Helmet>
-
             <Analytics />
         </div>
     );
--- src/routes/ThankYou.jsx
+++ src/routes/ThankYou.jsx
@@ -4,10 +4,32 @@ import { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { Analytics } from '@vercel/analytics/react';
+import {Helmet} from "react-helmet";
 
 const ThankYou = () => {
     return (
         <div className='w-screen h-screen flex flex-col justify-center items-center px-10 relative'>
+            <Helmet>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16749261252"></script>
+                <script type="text/javascript">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'AW-16749261252');
+                `}
+                </script>
+                <script type="text/javascript">
+                {`
+                    gtag('event', 'conversion', {
+                        'send_to': 'AW-16749261252/mfBOCLOu7-AZEMTj1bI-',
+                        'value': 1.0,
+                        'currency': 'USD'
+                    });
+                `}
+                </script>
+            </Helmet>
+
             <Navbar absolute={true} button={false}/>
             <div>
                 <h1 className='mb-5 text-4xl md:text-5xl font-medium text-center'>Your're all set! 🎉✨</h1>
