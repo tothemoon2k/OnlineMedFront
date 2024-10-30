ff0313e7a7ca9d5548c13885ddfa6e2dfeceb404
Justin Garner <justin2013pdx@gmail.com>
2024-10-30 18:03:46 -0400

    Add v3 beta form

--- src/routes/Redirect.jsx
+++ src/routes/Redirect.jsx
@@ -1,40 +1,16 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {Helmet} from "react-helmet";
 import { Analytics } from '@vercel/analytics/react';
 
 const Redirect = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        window.location.href = 'https://xhj91lsy6tk.typeform.com/to/MTsMbU18';
+        window.location.href = 'https://xhj91lsy6tk.typeform.com/to/PwQVNSXI';
     }, [navigate]);
 
     return (
         <div>
-            <Helmet>
-                <title>Redirecting...</title>
-                <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16749261252"></script>
-                <script type="text/javascript">
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', 'AW-16749261252');
-                `}
-                </script>
-                <script type="text/javascript">
-                {`
-                    gtag('event', 'conversion', {
-                        'send_to': 'AW-16749261252/mfBOCLOu7-AZEMTj1bI-',
-                        'value': 1.0,
-                        'currency': 'USD'
-                    });
-                `}
-                </script>
-            </Helmet>
-
             <Analytics />
         </div>
     );
