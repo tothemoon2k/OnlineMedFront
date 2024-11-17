dd6c144e990129bcf81af2be73190ec674594573
Justin Garner <justin2013pdx@gmail.com>
2024-11-17 18:08:51 -0500

    Add livechat widget

--- src/routes/Confirmed.jsx
+++ src/routes/Confirmed.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import Helmet from 'react-helmet';
+import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { Analytics } from '@vercel/analytics/react';
+import {Helmet} from "react-helmet";
+import { LiveChatWidget } from "@livechat/widget-react";
 
 const Confirmed = () => {
+    function handleNewEvent(event) {
+        console.log("LiveChatWidget.onNewEvent", event);
+    }
+
     return (
         <div className='w-screen h-screen flex flex-col justify-center items-center px-10 relative'>
+            <LiveChatWidget
+                license="18883233"
+                visibility="maximized"
+                onNewEvent={handleNewEvent}
+            />
+
             <Helmet>
                 <script type="text/javascript">
                 {`
@@ -17,27 +30,6 @@ const Confirmed = () => {
                     });
                 `}
                 </script>
-                <script type="text/javascript">
-                {`
-                    window.__lc = window.__lc || {};
-                    window.__lc.license = 18883233;
-                    window.__lc.integration_name = "manual_onboarding";
-                    window.__lc.product_name = "livechat";
-                    (function(n,t,c){
-                        function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}
-                        var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},
-                        once:function(){i(["once",c.call(arguments)])},
-                        off:function(){i(["off",c.call(arguments)])},
-                        get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},
-                        call:function(){i(["call",c.call(arguments)])},
-                        init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};
-                        !n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e
-                    }(window,document,[].slice));
-                `}
-                </script>
-                <noscript>
-                    <a href="https://www.livechat.com/chat-with/18883233/" rel="nofollow">Chat with us</a>, powered by <a href="https://www.livechat.com/?welcome" rel="noopener nofollow" target="_blank">LiveChat</a>
-                </noscript>
             </Helmet>
 
             <Navbar absolute={true} button={false}/>
