cedb7f7dff09ac9abb96abf8eace0f08c24a820a
Justin Garner <justin2013pdx@gmail.com>
2024-11-23 22:59:51 -0500

    Add opportunites banner

--- src/routes/Landing.jsx
+++ src/routes/Landing.jsx
@@ -20,14 +20,51 @@ import doc5 from "../../public/assets/doc5.webp";
 import nickJones from "../../public/assets/nickjones.png";
 import doctorsTalking from "../../public/assets/doctorsTalking.webp";
 import { handleCtaClick } from '../handleCtaClick';
+import { useState } from 'react';
 
 const Landing = () => {
+    const [showingBanner, setShowingBanner] = useState(true);
+
+    const handleApplyBtn = () => {
+        window.location.href = "mailto:business@onlinemed.tech?subject=Physician Telehealth Opportunities - OnlineMed";
+    }
+
+    const handelDismissBtn = () => {
+        setShowingBanner(false);
+    }
+
     <Helmet>
 6       <link rel="preload" as="image" href={bg} />
 7  </Helmet>
     return (
         <div className='w-screen h-fit relative overflow-hidden px-8 md:px-0'>
             <img className='w-screen absolute -z-10' src={bg} alt="Background pattern" />
+
+            {showingBanner &&
+                <div className="-ml-8 w-screen relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+                    <div className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl" aria-hidden="true">
+                        <div className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30" ></div>
+                    </div>
+                    <div className="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl" aria-hidden="true">
+                        <div className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30" ></div>
+                    </div>
+                    <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
+                        <p className="text-sm/6 text-gray-900">
+                        <strong className="font-medium">Jobs</strong><svg viewBox="0 0 2 2" className="mx-2 inline size-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>Are you a physician? Partner with us to provide flexible telehealth care.
+                        </p>
+                        <button onClick={handleApplyBtn} className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">Apply<span aria-hidden="true">&rarr;</span></button>
+                    </div>
+                    <div className="flex flex-1 justify-end">
+                        <button onClick={handelDismissBtn} type="button" className="-m-3 p-3 focus-visible:outline-offset-[-4px]">
+                            <span className="sr-only">Dismiss</span>
+                            <svg className="size-5 text-gray-900" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                                <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            }
+
             <Navbar />
 
             {/* Hero */}
